2ff32a5a4e76e654cc1226670c8086dfd3dc495e
vinod8833 <vinodkr8833@gmail.com>
2024-08-10 17:28:21 +0530

    add

--- food/settings.py
+++ food/settings.py
@@ -126,8 +126,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'viinodkumar34@gmail.com'
-EMAIL_HOST_PASSWORD = 'csbj wpke wywo rrmg'  # Be cautious with sensitive information
+EMAIL_HOST_USER = 'enter your email'
+EMAIL_HOST_PASSWORD = 'enter your email passcode'  # Be cautious with sensitive information
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
